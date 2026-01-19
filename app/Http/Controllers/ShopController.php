ac5205ec44f90c8142e99928beefc9e94cc3742d
Roxana <ms.roxanarahimi@gmail.com>
2026-01-19 15:10:43 +0330

    add

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\BaseProductResource;
 use App\Http\Resources\CategoryResource;
-use App\Http\Resources\ProductResource;
 use App\Models\BaseProduct;
 use App\Models\Category;
-use App\Models\Product;
+use App\Models\Order;
+use App\Models\OrderItem;
 use Illuminate\Http\Request;
 
 class ShopController extends Controller
@@ -21,6 +21,7 @@ class ShopController extends Controller
             return $exception;
         }
     }
+
     public function products(Request $request)
     { //where: categories, stock, off---- sort: new,sale,productsrice
         try {
@@ -28,25 +29,30 @@ class ShopController extends Controller
             $products = BaseProduct::orderByDesc('id');
 
             if ($request['category_ids']) {
-                $products = $products->whereIn('category_id',array_map('intval', explode(',', $request['category_ids'])) );
+                $products = $products->whereIn('category_id', array_map('intval', explode(',', $request['category_ids'])));
             }
             if ($request['stock'] == 'true') {
                 $products = $products->whereHas('products', function ($query) {
                     $query->where('stock', '>', 0);
                 });
             }
-            if ($request['off'] == 'true') {
-                $products = $products->whereHas('products', function ($query) use ($request) {
-                    $query->where('off', '>', 0);
-                })->orWhere('off', '>', 0);
-            }
 
-            if ($request['term']&& count_chars($request['term']>=3)) {
+            if ($request['term'] && count_chars($request['term'] >= 3)) {
                 $products = $products->where('title', 'like', '%' . $request['term'] . '%');
             }
+            if ($request['brand'] && count_chars($request['brand'] >= 3)) {
+                $products = $products->where('title', 'like', '%' . $request['brand'] . '%');
+            }
 
+            if ($request['off'] == 'true') {
+                $products = $products->where('off', '>', 0)->orWhereHas('products', function ($query) {
+                    $query->where('off', '>', 0);
+                });
+            }
+//
 //            if ($request['sort'] == 'sale') {
-//                $products = $products->orderByDesc('sale');
+//                $products->withSum('products', 'sale')
+//                    ->orderByDesc('products_sum_sale');
 //            }
 //            if ($request['sort'] == 'cheap') {
 //                $products = $products->orderBy('price');
@@ -67,7 +73,7 @@ class ShopController extends Controller
     {
         try {
             $products = BaseProduct::orderByDesc('id'); //special?
-            if($request['category_id']) {
+            if ($request['category_id']) {
                 $products = $products->where('category_id', $request['category_id']);
             }
             $products = $products->take(4)->get();
@@ -78,4 +84,49 @@ class ShopController extends Controller
         }
     }
 
+    public function updateCart(Request $request)
+    {
+        try {
+            $cart = Order::where('user_id', $request['user_id'])->where('type', 'cart')->first();
+            foreach ($request->orderItems as $item) {
+                $orderItem = OrderItem::where('order_id', $cart['id'])->where('product_id', $item['product_id'])->first();
+                if ($orderItem) {
+                    $orderItem->update(['quantity' => $item['quantity']]);
+                } else {
+                    OrderItem::create([
+                        'order_id' => $cart['id'],
+                        'product_id' => $item['product_id'],
+                        'quantity' => $item['quantity'],
+                    ]);
+                }
+                $items = array_column($request['orderItems'], 'product_id');
+                $remove = OrderItem::where('order_id', $cart['id'])->whereNotIn('product_id', $items)->get();
+                $remove->each->delete();
+            }
+            return $cart;
+        } catch (\Exception $exception) {
+            return $exception;
+        }
+    }
+
+    public function addToCart(Request $request)
+    {
+        try {
+            $cart = Order::where('user_id', $request['user_id'])->where('type', 'cart')->first();
+            $orderItem = OrderItem::where('order_id', $cart['id'])->where('product_id', $request['product_id'])->first();
+            if ($orderItem) {
+                $orderItem->update(['quantity' => $orderItem['quantity'] + $request['quantity']]);
+            } else {
+                OrderItem::create([
+                    'order_id' => $cart['id'],
+                    'product_id' => $request['product_id'],
+                    'quantity' => $request['quantity'],
+                ]);
+            }
+            $cart = Order::findOrFail($cart['id']);
+            return response($cart, 200);
+        } catch (\Exception $exception) {
+            return $exception;
+        }
+    }
 }
--- database/migrations/2025_12_03_204312_create_orders_table.php
+++ database/migrations/2025_12_03_204312_create_orders_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->string('code')->unique()->nullable();
+            $table->string('type')->default('cart');
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->foreignId('address_id')->constrained('addresses')->cascadeOnDelete();
             $table->integer('total_amount');
