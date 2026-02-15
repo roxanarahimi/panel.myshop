4c9350a7fa9aaf6aabcb777fc85f8de9f781ded1
Roxana <ms.roxanarahimi@gmail.com>
2026-02-15 15:55:53 +0330

    Revert "add"
    This reverts commit 8366f7839c054b8d64032b63bcaf48e7e33ee0aa.

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -14,6 +14,11 @@ class CategoryResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return[
+            "id" => $this->id,
+            "title" => $this->title,
+            "products" => BaseProductResource::collection($this->products),
+
+        ];
     }
 }
