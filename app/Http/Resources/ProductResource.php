1a1e2866aa56b2d05a793dfe4e24058cd618edac
Roxana <ms.roxanarahimi@gmail.com>
2026-02-15 15:38:49 +0330

    add

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -14,14 +14,6 @@ class ProductResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return [
-            "id" => $this->id,
-            "base_product_id" => $this->base_product_id,
-            "size" => $this->size,
-            "info" => $this->info,
-            "price" => min($this->price,$this->info->price),
-            "off" => max($this->off,$this->info->off),
-            "category_id" => $this->info->category_id,
-        ];
+        return parent::toArray($request);
     }
 }
