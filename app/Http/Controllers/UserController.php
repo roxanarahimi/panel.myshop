607a1e0b6005479ba2fdb439fbe848dbdffc1571
Roxana <ms.roxanarahimi@gmail.com>
2026-09-23 02:22:12 +0330

    add

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -79,7 +79,7 @@ class UserController extends Controller
 
 
             $array = json_decode($result, true);
-//            return response([$result,$array],500);
+            return response([$result,$array],500);
 
             if ($result) {
                 $info = [
