cbf91d7d7c2a48309f61d86103d68becb1f4f4bb
Roxana <ms.roxanarahimi@gmail.com>
2026-09-25 21:08:04 +0330

    add

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -46,9 +46,8 @@ class PaymentController extends Controller
         try {
 
 //        بررسی وضعیت تراکنش | Verify payment status
-            return response($request,400);
-            $authority = $request->query('Authority');// دریافت کوئری استرینگ ارسال شده توسط زرین پال
-            $status = $request->query('Status');// دریافت کوئری استرینگ ارسال شده توسط زرین پال
+            $authority = $request->Authority;// دریافت کوئری استرینگ ارسال شده توسط زرین پال
+            $status = $request->Status;// دریافت کوئری استرینگ ارسال شده توسط زرین پال
 
 
             $response = zarinpal()
