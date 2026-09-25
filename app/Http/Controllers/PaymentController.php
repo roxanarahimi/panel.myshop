17b789cb43cf25eb3e8e9fdede7dacba962f8891
Roxana <ms.roxanarahimi@gmail.com>
2026-09-25 19:13:12 +0330

    add

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -30,7 +30,7 @@ class PaymentController extends Controller
 // $response->authority();
 
 // هدایت مشتری به درگاه پرداخت
-        return response($response);
+        return response($response,$request->getCode());
     }
 
 
